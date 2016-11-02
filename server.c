cbb1905faae1792300d5c79ccb6a1dd14cbbe46f
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-11-02 01:16:44 -0600

    major changes on server.c
    worked on code a bit, still alot unfinished and not very optimized :-^(

--- server.c
+++ server.c
@@ -11,45 +11,111 @@
 
 #include "shared.h"
 
+//Should be writing to log instead of STDERR
+int send_message(int s, void* address, int byte_length){
+	if(send(s, address, byte_length, 0) <= 0){
+		perror("Server: failed to send message");
+		return -1;
+	}
+	return 1;
+}
+
+int receive_message(int s, void* address, int byte_length){
+	if(recv(s, address, byte_length, 0) <= 0){
+		perror("Server: failed to receive message");
+		return -1;
+	}
+	return 1;
+}
+
 int main(int argc, char* argv[])
 {	
-	//Daemon stuff should go here?
-	
+	//Daemon stuff should go here? ctrl+e reminder
+	//~ pid_t pid = 0;
+    //~ pid_t sid = 0;
+    //~ pid = fork();
+    //~ if (pid < 0){
+        //~ perror("fork failed!\n");
+        //~ exit(1);
+    //~ }
+    //~ if (pid > 0){
+       //~ printf("pid of child process %d \n", pid);
+       //~ exit(0); 
+    //~ }
+    //~ umask(0);
+	//~ // open a log file
+    //~ fp = fopen ("logfile.log", "w+");
+    //~ if(!fp){
+    	//~ printf("cannot open log file");
+    //~ }
+    //~ // create new process group
+    //~ sid = setsid();
+    //~ if(sid < 0)
+    //~ {
+    	//~ fprintf(fp, "cannot create new process group");
+        //~ exit(1);
+    //~ }
+    //~ /* Change the current working directory */ 
+    //~ if ((chdir("/")) < 0) {
+      //~ printf("Could not change working directory to /\n");
+      //~ exit(1);
+    //~ }
+	//~ // close standard fds
+    //~ close(STDIN_FILENO);
+    //~ close(STDOUT_FILENO);
+    //~ close(STDERR_FILENO);
+
 	//Initializing stuff
-	int	sock, fromlength, snew;
+	int	sock, fromlength, snew, i, j, nbytes, bool;
 	unsigned short tempNum;
-	int PORT = atoi(argv[1]);
+	char tempChar;
 	struct	sockaddr_in	master, from;
+	
+	//Set up example username list
+	node* headNode = (node*)malloc(sizeof(node));
+	headNode->next = NULL;
+	node* currentNode;
+	
+	char* nameBuffer = (char*)malloc(256);
+    if(nameBuffer == NULL){
+		perror ("Server: failed to allocate enough memory");
+		exit (1);
+	}
+	char* messageBuffer = (char*)malloc(sizeof(char)*65536);
+    if(messageBuffer == NULL){
+		perror ("Client: failed to allocate enough memory");
+		exit (1);
+	}
 	sock = socket (AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
 		perror ("Server: cannot open master socket");
 		exit (1);
 	}
+	memset(&master, 0, sizeof master);
 	master.sin_family = AF_INET;
-	master.sin_addr.s_addr = INADDR_ANY;
-	master.sin_port = htons (PORT);
+	master.sin_addr.s_addr = inet_addr("127.0.0.1");//replaced INADDR_ANY
+	master.sin_port = htons (atoi(argv[1]));
 	if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
 		perror ("Server: cannot bind master socket");
 		exit (1);
 	}
 	if (listen(sock, 10) == -1) {
-        perror("listen");
+        perror("Server: listen failed");
         exit(-1);
     }
-	fromlength = sizeof (from);
-	snew = accept (sock, (struct sockaddr*) & from, & fromlength);
-	if (snew < 0) {
-		perror ("Server: accept failed");
-		exit (1);
-	}
-	//Done Initialization
 	
-	printf("I'm here!\n");
+	//Done Initialization
 	
 	//Set up signal handlers
 	void signal_handler(int sig){
 		perror("Terminating...");
-		close(snew);
+		//Close all sockets connected to the server
+		close(sock);
+		currentNode = headNode;
+		while(currentNode->next!= NULL){
+			close(currentNode->fd);
+			currentNode = currentNode->next;
+		}
 		exit(0);
 	}
 	struct sigaction segv;
@@ -58,45 +124,107 @@ int main(int argc, char* argv[])
 	segv.sa_flags = 0;
 	sigaction(SIGINT,&segv,0);
 	
-	//Set up example username list
-	node* headNode = (node*)malloc(sizeof(node));
-	headNode->next = NULL;
-	addName(headNode,"Jerry");
-	addName(headNode,"Gary");
-	addName(headNode,"Barry");
+	//Initialize select and variables/structures it needs
+	fd_set master_fds;    // master file descriptor list
+    fd_set read_fds;  // temp file descriptor list for select()
+    int fdmax = sock;        // maximum file descriptor number
+    int yes=1;        // for setsockopt() SO_REUSEADDR, below
+    FD_ZERO(&master_fds);
+    FD_ZERO(&read_fds);
+	FD_SET(sock, &master_fds);
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof optval);
 	
-	char* nameBuffer = (char*)malloc(256);
-    if(nameBuffer == NULL){
-		perror ("Client: failed to allocate enough memory");
-		exit (1);
-	}
+	// main loop for original server.
+    while(1){
+        read_fds = master_fds;
+        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+            perror("select");
+            exit(-1);
+        }
+
+        // run through the existing connections looking for data to read
+        for(i = 0; i <= fdmax; i++) {
+            if (FD_ISSET(i, &read_fds)) { // we got one!!
+                if (i == listener) {
+                    // handle new connections
+                    fromlength = sizeof (from);
+					snew = accept (sock, (struct sockaddr*) & from, & fromlength);
+					if (snew < 0) {
+						perror ("Server: accept failed");
+						exit (1);
+					}else{
+                        //printf("selectserver: new connection from %s:%d on socket %d\n", inet_ntoa(remoteaddr.sin_addr), ntohs(remoteaddr.sin_port), newfd);
+                        //Initial handshake
+						tempNum = 0xCFA7;
+						bool = send_message (snew, &tempNum, 2); //Assumes send() will return -1 if socket is disconnected
+						tempNum = htons(nameCount(headNode));
+						bool = send_message (snew, &tempNum, 2);
+						currentNode = headNode;
+						while(currentNode->next!=NULL){
+							tempChar = strlen(currentNode->name);
+							bool = send_message(snew, &tempChar, 1);
+							bool = send_message(snew, currentNode->name, tempChar);
+							currentNode = currentNode->next;
+						}
+						//If no error received from sending in socket...
+						if(bool != -1){
+							receive_message(snew, &tempChar, 1);
+							tempNum = tempChar;
+							receive_message(snew, nameBuffer, tempNum);
+							//End handshake
+							//Now add it to the server's data structures
+							addNameServer(headNode,nameBuffer,snew);
+							printNames(headNode); //just a test ;)
+							FD_SET(snew, &master_fds); // add to master set
+							if (snew > fdmax) {    // keep track of the max
+								fdmax = snew;
+							}
+						}
+                    }
+                } else {
+                    // handle data from a client -- THIS PART ISNT DONE YET
+                    recv(i, buf, 2, 0);
+                    if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
+                        // got error or connection closed by client
+                        if (nbytes == 0) {
+                            // connection closed -- should write to a log instead of STDOUT here
+                            printf("Server: socket %d hung up\n", i);
+                        } else {
+                            perror("Server: Error reading from socket");
+                        }
+                        //close socket and remove from master file descriptor
+                        close(i);
+                        FD_CLR(i, &master_fds);
+                        //remove node from list of nodes
+                        currentNode = removeFd(headNode,i);
+                        if (nodeHead == currentNode){
+							nodeHead = nodeHead->next;
+						}
+						free(currentNode);
+                    } else {
+                        // we got some data from a client
+                        for(j = 0; j <= fdmax; j++) {
+                            // send to everyone!
+                            if (FD_ISSET(j, &master)) {
+                                // except the listener and ourselves
+                                if (j != listener && j != i) {
+                                    if (send(j, buf, nbytes, 0) == -1) {
+                                        perror("send");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } // END handle data from client
+            } // END got new incoming connection
+        } // END looping through file descriptors
+    } // END while()
 	
-	printf("I'm here!\n");
+	return 0;
 	
-	//Begin handshake
-	tempNum = 0xCFA7;
-	send (snew, &tempNum, 2, 0);
-	tempNum = htons(nameCount(headNode));
-	send (snew, &tempNum, 2, 0);
-	node* currentNode = headNode;
-	char tempChar;
-	while(currentNode->next!=NULL){
-		tempChar = strlen(currentNode->name);
-		send(snew, &tempChar, 1, 0);
-		send(snew, currentNode->name, tempChar, 0);
-		currentNode = currentNode->next;
-	}
-	recv(snew, &tempChar, 1, 0);
-	tempNum = tempChar;
-	recv(snew, nameBuffer, tempNum, 0);
-	addName(headNode,nameBuffer);
-	printNames(headNode); //just a test ;)
-	//End handshake
 	
-	while(1){//Testing purposes... wait for signalC
-		printf("Your orders Master...\n");
-		sleep(1);
-	}
+	
+	
 	
 	//NOTE!!!!!!!!!
 	//This is a copy paste from demo2... will revise!
@@ -123,86 +251,4 @@ int main(int argc, char* argv[])
     //~ FD_ZERO(&read_fds);
 //~ 
 	//~ listener = socket(AF_INET, SOCK_STREAM, 0);
-	//~ 
-    //~ // get us a socket and bind it
-    //~ memset(&sa, 0, sizeof sa);
-    //~ sa.sin_family = AF_INET;
-    //~ sa.sin_addr.s_addr = inet_addr("127.0.0.1");
-    //~ sa.sin_port = htons(MY_PORT);
-           //~ 
-    //~ if (bind(listener, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
-        //~ perror("bind");
-    	//~ exit(-1);
-    //~ }
-//~ 
-    //~ // listen
-    //~ if (listen(listener, 10) == -1) {
-        //~ perror("listen");
-        //~ exit(-1);
-    //~ }
-//~ 
-    //~ // add the listener to the master set
-    //~ FD_SET(listener, &master);
-//~ 
-    //~ // keep track of the biggest file descriptor
-    //~ fdmax = listener; // so far, it's this one
-//~ 
-    //~ // main loop
-    //~ for(;;) {
-        //~ read_fds = master; // copy it
-        //~ if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
-            //~ perror("select");
-            //~ exit(-1);
-        //~ }
-//~ 
-        //~ // run through the existing connections looking for data to read
-        //~ for(i = 0; i <= fdmax; i++) {
-            //~ if (FD_ISSET(i, &read_fds)) { // we got one!!
-                //~ if (i == listener) {
-                    //~ // handle new connections
-                    //~ addrlen = sizeof remoteaddr;
-                    //~ newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
-//~ 
-                    //~ if (newfd == -1) {
-                        //~ perror("accept");
-                    //~ } else {
-                        //~ FD_SET(newfd, &master); // add to master set
-                        //~ if (newfd > fdmax) {    // keep track of the max
-                            //~ fdmax = newfd;
-                        //~ }
-                        //~ printf("selectserver: new connection from %s:%d on socket %d\n",
-                            //~ inet_ntoa(remoteaddr.sin_addr), ntohs(remoteaddr.sin_port), newfd);
-                    //~ }
-                //~ } else {
-                    //~ // handle data from a client
-                    //~ if ((nbytes = recv(i, buf, sizeof buf, 0)) <= 0) {
-                        //~ // got error or connection closed by client
-                        //~ if (nbytes == 0) {
-                            //~ // connection closed
-                            //~ printf("selectserver: socket %d hung up\n", i);
-                        //~ } else {
-                            //~ perror("recv");
-                        //~ }
-                        //~ close(i); // bye!
-                        //~ FD_CLR(i, &master); // remove from master set
-                    //~ } else {
-                        //~ // we got some data from a client
-                        //~ for(j = 0; j <= fdmax; j++) {
-                            //~ // send to everyone!
-                            //~ if (FD_ISSET(j, &master)) {
-                                //~ // except the listener and ourselves
-                                //~ if (j != listener && j != i) {
-                                    //~ if (send(j, buf, nbytes, 0) == -1) {
-                                        //~ perror("send");
-                                    //~ }
-                                //~ }
-                            //~ }
-                        //~ }
-                    //~ }
-                //~ } // END handle data from client
-            //~ } // END got new incoming connection
-        //~ } // END looping through file descriptors
-    //~ } // END for(;;)--and you thought it would never end!
-	//~ 
-	return 0;
 }
