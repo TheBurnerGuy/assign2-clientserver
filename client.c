5331a679ee54be79cb2f2285304f3436f230a21f
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-11-02 23:16:59 -0600

    mistake, reverted
    tit,.e

--- client.c
+++ client.c
@@ -1,293 +1,238 @@
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
 #include <string.h>
+#include <sys/time.h>
+#include <signal.h>
 
 #include "shared.h"
 
-//send_message()
-//sends message to specified socket while checking for errors
-//returns 0 if socket closed or error occurred, returns 1 if send was successful
-//Should be writing to log instead of STDERR
-int send_message(int s, void* address, int byte_length){
+#define STDIN 0
+
+//Sends username in the form of length string
+void send_message(int s, void* address, int byte_length){
 	if(send(s, address, byte_length, 0) <= 0){
-		perror("Server: failed to send message");
-		return 0;
+		perror("Client: failed to send message");
+		raise(SIGINT);
 	}
-	return 1;
 }
 
-int receive_message(int s, void* address, int byte_length){
+void receive_message(int s, void* address, int byte_length){
 	if(recv(s, address, byte_length, 0) <= 0){
-		perror("Server: failed to receive message");
-		return 0;
+		perror("Client: failed to receive message");
+		raise(SIGINT);
 	}
-	return 1;
 }
 
 int main(int argc, char* argv[])
-{	
-	//Daemon stuff should go here? ctrl+e reminder
-	//~ pid_t pid = 0;
-    //~ pid_t sid = 0;
-    //~ pid = fork();
-    //~ if (pid < 0){
-        //~ perror("fork failed!\n");
-        //~ exit(1);
-    //~ }
-    //~ if (pid > 0){
-       //~ printf("pid of child process %d \n", pid);
-       //~ exit(0); 
-    //~ }
-    //~ umask(0);
-	//~ // open a log file
-    //~ fp = fopen ("logfile.log", "w+");
-    //~ if(!fp){
-    	//~ printf("cannot open log file");
-    //~ }
-    //~ // create new process group
-    //~ sid = setsid();
-    //~ if(sid < 0)
-    //~ {
-    	//~ fprintf(fp, "cannot create new process group");
-        //~ exit(1);
-    //~ }
-    //~ /* Change the current working directory */ 
-    //~ if ((chdir("/")) < 0) {
-      //~ printf("Could not change working directory to /\n");
-      //~ exit(1);
-    //~ }
-	//~ // close standard fds
-    //~ close(STDIN_FILENO);
-    //~ close(STDOUT_FILENO);
-    //~ close(STDERR_FILENO);
-
-	//Initializing stuff
-	int	sock, fromlength, snew, i, j, nbytes, bool;
-	unsigned short tempNum;
-	unsigned char tempChar;
-	struct	sockaddr_in	master, from;
+{
+	int	s;
+	unsigned short number; //Used as a temporary variable, then the current users count
+	int PORT = atoi(argv[2]);
+	struct	sockaddr_in	server;
+	unsigned long host = inet_addr (argv[1]); //0.0.0.0
 	
-	//Set up example username list
-	node* headNode = (node*)malloc(sizeof(node));
-	headNode->next = NULL;
-	node* currentNode;
-	
-	char* nameBuffer = (char*)malloc(256);
-    if(nameBuffer == NULL){
-		perror ("Server: failed to allocate enough memory");
-		exit (1);
-	}
+	//Initializing variables not related to socket initialization
+	int i, stringLength;
+	node* nodeHead = (node*)malloc(sizeof(node));
+	nodeHead->next = NULL;
+	char temp;
 	char* messageBuffer = (char*)malloc(sizeof(char)*65536);
     if(messageBuffer == NULL){
 		perror ("Client: failed to allocate enough memory");
 		exit (1);
 	}
-	sock = socket (AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		perror ("Server: cannot open master socket");
+	char* nameBuffer = (char*)malloc(sizeof(char)*256);
+    if(nameBuffer == NULL){
+		perror ("Client: failed to allocate enough memory");
 		exit (1);
 	}
-	memset(&master, 0, sizeof master);
-	master.sin_family = AF_INET;
-	master.sin_addr.s_addr = inet_addr("127.0.0.1");//replaced INADDR_ANY
-	master.sin_port = htons (atoi(argv[1]));
-	if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
-		perror ("Server: cannot bind master socket");
-		exit (1);
-	}
-	if (listen(sock, 10) == -1) {
-        perror("Server: listen failed");
-        exit(-1);
-    }
+    unsigned short charCount = 0;
+    node* deleteNode;
 	
-	//Done Initialization
-	
-	//Set up signal handlers
+	//Set up signal handler function - used to terminate if handshake/messaging fails
 	void signal_handler(int sig){
-		perror("Terminating...");
-		//Close all sockets connected to the server
-		close(sock);
-		currentNode = headNode;
-		while(currentNode->next!= NULL){
-			close(currentNode->fd);
-			currentNode = currentNode->next;
-		}
+		close(s);
+		fprintf(stderr, "Terminating...\n");
+		deleteNodes(nodeHead);
 		exit(0);
 	}
+	//Set up signal handlers
 	struct sigaction segv;
 	segv.sa_handler = signal_handler;
 	sigemptyset(&segv.sa_mask);
 	segv.sa_flags = 0;
 	sigaction(SIGINT,&segv,0);
 	
-	//Initialize select and variables/structures it needs
-	fd_set master_fds;    // master file descriptor list
-    fd_set read_fds;  // temp file descriptor list for select()
-    int fdmax = sock;        // maximum file descriptor number
-    int yes=1;        // for setsockopt() SO_REUSEADDR, below
-    FD_ZERO(&master_fds);
-    FD_ZERO(&read_fds);
-	FD_SET(sock, &master_fds);
-	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof optval);
+	//Start connecting
+	s = socket (AF_INET, SOCK_STREAM, 0);
+	if (s < 0) {
+		perror ("Client: cannot open socket");
+		exit (1);
+	}
+	bzero (&server, sizeof (server));
+	server.sin_addr.s_addr = host;
+	server.sin_family = AF_INET;
+	server.sin_port = htons (PORT);
+	if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
+		perror ("Client: cannot connect to server");
+		exit (1);
+	}
+	//Finished Connecting
 	
-	// main loop for original server.
-    while(1){
-        read_fds = master_fds;
-        if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
-            perror("select");
-            exit(-1);
-        }
-
-        // run through the existing connections looking for data to read
-        for(i = 0; i <= fdmax; i++) {
-            if (FD_ISSET(i, &read_fds)) { // we got one!!
-                if (i == listener) {
-                    // handle new connections
-                    fromlength = sizeof (from);
-					snew = accept (sock, (struct sockaddr*) & from, & fromlength);
-					if (snew < 0) {
-						perror ("Server: accept failed");
-						//exit(1);
-					}else{
-                        //printf("selectserver: new connection from %s:%d on socket %d\n", inet_ntoa(remoteaddr.sin_addr), ntohs(remoteaddr.sin_port), newfd);
-                        //Initial handshake
-						tempNum = 0xCFA7;
-						bool = send_message (snew, &tempNum, 2); //Assumes send() will return -1 if socket is disconnected
-						tempNum = htons(nameCount(headNode));
-						bool = send_message (snew, &tempNum, 2);
-						currentNode = headNode;
-						while(currentNode->next!=NULL){
-							tempChar = strlen(currentNode->name);
-							bool = send_message(snew, &tempChar, 1);
-							bool = send_message(snew, currentNode->name, tempChar);
-							currentNode = currentNode->next;
-						}
-						//If no error received from sending in socket...
-						if(bool != 0){
-							receive_message(snew, &tempChar, 1);
-							tempNum = tempChar;
-							receive_message(snew, nameBuffer, tempNum);
-							//End handshake
-							//Now add it to the server's data structures
-							addNameServer(headNode,nameBuffer,snew);
-							printNames(headNode); //just a test ;)
-							FD_SET(snew, &master_fds); // add to master set
-							if (snew > fdmax) {    // keep track of the max
-								fdmax = snew;
-							}
-							//Send user connected message
-						}
-                    }
-                } else {
-                    // handle data from a client
-					//Implementing this part without using fork for now, until can figure out a good fork implementation
-					//Personal notes:
-					//Advantages for using fork(): Doesn't have to search for socket's name using socket fd
-					//Disadvantages: Have to keep each child process separate... (definitely possible)
-                    if (receive_message(i, &tempNum, 2) <= 0) {
-                        // got error or connection closed by client
-                        if (tempNum == 0) {
-                            // connection closed -- should write to a log instead of STDOUT here
-                            printf("Server: socket %d hung up\n", i);
-                        } else {
-                            perror("Server: Error reading from socket");
-                        }
-                        //close socket and remove from master file descriptor
-                        close(i);
-                        FD_CLR(i, &master_fds);
-						//if i was fdmax, use fdmax-1
-						if (fdmax == i) --fdmax;
-                        //remove node from list of nodes
-                        currentNode = removeFd(headNode,i);
-                        if (nodeHead == currentNode){
-							nodeHead = nodeHead->next;
-						}
-						free(currentNode);
-						//Send termination message to all clients
-                    } else {
-                        // we got data from a client
-						receive_message(i, messageBuffer, tempNum);
-						currentNode = findFd(headNode, i);
-						nameBuffer = currentNode->name;
-						tempChar = strlen(nameBuffer);
-                        for(j = 0; j <= fdmax; j++) {
-                            // send to everyone!
-                            if (FD_ISSET(j, &master)) {//is this really needed?
-                                // except the listener and ourselves
-                                if (j != listener && j != i) {
-									//Send username length
-									//Can probably reduce these 4 repetitions to a function--> will do later on
-									if (send_message(j, tempChar, 1) == 0) {
-                                        perror("send");
-										continue;
-										//might try to terminate connection here? Will test later
-                                    }
-									//Send username
-									if (send_message(j, nameBuffer, tempChar) == 0) {
-                                        perror("send");
-										continue;
-										//might try to terminate connection here? Will test later
-                                    }
-									//Send message length
-									if (send_message(j, tempNum, 2) == 0) {
-                                        perror("send");
-										continue;
-										//might try to terminate connection here? Will test later
-                                    }
-									//Send message
-                                    if (send_message(j, messageBuffer, tempNum) == 0) {
-                                        perror("send");
-										continue;
-										//might try to terminate connection here? Will test later
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } // END handle data from client
-            } // END got new incoming connection
-			if(){//need to implement time here
-				
-			}//END checking if process is idle
-        } // END looping through file descriptors
-    } // END while()
+	//Handshake begin
+	receive_message(s, &number, sizeof (number));
+	if (number != 0xCFA7){
+		perror ("Client: failed to connect to server");
+		exit (1);
+	}
+	//fprintf (stderr, "Process %d gets number %d\n", getpid (),
+	//ntohl (number));
 	
-	return 0;
+	receive_message(s, &number, sizeof (number));
+	number = ntohs(number);
+	//Check if there are other users in server
+	if(number != 0){
+		for(i = 0; i<number; ++i){
+			receive_message(s,&stringLength,1);
+			receive_message(s,nameBuffer,stringLength);
+			addName(nodeHead,nameBuffer);
+		}
+	}
+	//Finally, send own username
+	number = strlen(argv[3]);
+	temp = number;
+	send_message(s,&temp,1);
+	send_message(s,argv[3],number);
+	//Handshake end
+	printNames(nodeHead);
 	
+	//Initialize some functions/signal handlers before entering select()
 	
+	//User log - used to report user-update messages (otherwise pointless)
+	FILE* userlog;
+	userlog = fopen("userlog.txt","w");
 	
+	int alarmBool = 0;
+	//Alarm signal handler function
+	void alarm_handler(int sig){ alarmBool = 1;}
+	alarm(9); //Initialize alarm
 	
+	//Set up alarm signal handler
+	struct sigaction sega;
+	sega.sa_handler = alarm_handler;
+	sigemptyset(&sega.sa_mask);
+	sega.sa_flags = 0;
+	sigaction(SIGALRM,&sega,0);
 	
-	//NOTE!!!!!!!!!
-	//This is a copy paste from demo2... will revise!
-	//Code below is not functional, do not use!
-	//NOTE!!!!!
-
-	//~ fd_set master;    // master file descriptor list
-    //~ fd_set read_fds;  // temp file descriptor list for select()
-    //~ int fdmax;        // maximum file descriptor number
-//~ 
-    //~ int listener;     // listening socket descriptor
-    //~ int newfd;        // newly accept()ed socket descriptor
-    //~ struct sockaddr_in sa; 
-    //~ struct sockaddr_in remoteaddr; // client address
-    //~ socklen_t addrlen;
-//~ 
-    //~ char buf[256];    // buffer for client data
-    //~ int nbytes;
-    //~ 
-    //~ int yes=1;        // for setsockopt() SO_REUSEADDR, below
-    //~ int i, j, rv;
-//~ 
-    //~ FD_ZERO(&master);    // clear the master and temp sets
-    //~ FD_ZERO(&read_fds);
-//~ 
-	//~ listener = socket(AF_INET, SOCK_STREAM, 0);
+	//Set up select
+	struct timeval tv;
+    fd_set readfds;
+    fd_set writefds;
+    tv.tv_usec = 500000;
+	
+	while(1){
+		tv.tv_sec = 2;
+		FD_ZERO(&readfds);
+		FD_SET(s, &readfds);
+		FD_SET(STDIN, &readfds);
+		
+		if(select(s+1,&readfds, NULL, NULL, &tv) == -1){
+			perror("Client: Error while waiting for input/receiving output");
+			raise(SIGINT);
+		}
+		//Was a message received?
+		if(FD_ISSET(s, &readfds)){
+			receive_message(s,&temp,1);
+			if(temp==0){
+				//User message received -- might have error since unsigned short is supposed to be 2 bytes long?
+				receive_message(s, &temp, 1);
+				receive_message(s, nameBuffer, temp);
+				printf("%s: ",nameBuffer);
+				receive_message(s, &number, 2);
+				number = ntohs(number);
+				//Special case: Message of length 0, write to a userlog
+				if(number == 0){
+					number = strlen(nameBuffer);
+					for(i = 0; i<number; ++i){
+						fputc(nameBuffer[i],userlog);
+					}
+					number = 0;
+				}
+				//Normal case: Normal message
+				else{ //number!= 0
+					receive_message(s, &messageBuffer,number);
+					printf("%s",messageBuffer);
+					printf("\n");
+				}
+			}else if(temp==1){
+				//New user signal received
+				receive_message(s, &number, 1);
+				receive_message(s, nameBuffer, number);
+				addName(nodeHead,nameBuffer);
+			}else if(temp==2){
+				//User left signal received
+				receive_message(s, &number, 1);
+				receive_message(s, nameBuffer, number);
+				deleteNode = removeName(nodeHead,nameBuffer); //value returned to deleteNode is the node with the username found with removeName()
+				if (nodeHead->name == deleteNode->name){
+					nodeHead = nodeHead->next;
+				}
+				free(deleteNode);
+			}else{
+				perror("Received unknown message.\n");
+				exit(1);
+			}
+			continue; //Prioritise reading over sending message. Don't want send() to block while messages are waiting to be read.
+		}//End FD_ISSET readfds
+		//If there's a message entered into STDIN, read it into messageBuffer
+		if(FD_ISSET(STDIN, &readfds)){
+			charCount = 0;
+			while(1){//waiting for temp == 10
+				receive_message(STDIN,&temp,1);
+				if(temp == 10){
+					break;
+				}
+				messageBuffer[charCount] = temp;
+				++charCount;
+			}
+			//Now write it.
+			//Special case: It's a command, beginning with a /
+			if(messageBuffer[0]=='/' && charCount == 6){
+				//Slightly complex code to find if messageBuffer is equal to "/users"
+				for(i = 0; i< charCount; ++i){
+					if(messageBuffer[i]!="/users"[i]) break;
+				}
+				if(i==6){
+					printNames(nodeHead);
+				}
+				else{
+					printf("Command not found.");
+				}
+			}else if(messageBuffer[0]=='/'){
+				printf("Command not found.");
+			//Normal Case:
+			}else{
+				charCount = htons(charCount);
+				send_message(s,&charCount,2);
+				send_message(s, messageBuffer, ntohs(charCount));
+				alarmBool = 0;
+				alarm(9);
+			}
+		}//End FD_ISSET STDIN
+		//If no message has been sent in the past 9 seconds, send an idle message
+		if(alarmBool){
+			fprintf(stderr, "Sending idle message\n"); //Testing
+			number = htons(0);
+			send_message(s,&number,2);
+			alarmBool = 0;
+			alarm(9);
+		}
+	}
+	close (s);
 }
+0
